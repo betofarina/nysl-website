26e03a4b423b258b944a51183d0bf9a8f33acf1b
betofarina <32700183+betofarina@users.noreply.github.com>
2017-10-11 11:02:53 +0200

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,11 @@
+
+var elGenderStyle = document.getElementById('css_Link');
+
+function changeStyleM() {
+  elGenderStyle.href = 'genderm.css';
+};
+function changeStyleF() {
+  elGenderStyle.href = 'genderf.css';  
+};
+
+
